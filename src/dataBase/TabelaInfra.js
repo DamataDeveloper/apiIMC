9f7e383a900ff149f4483310101d471d60f1fe7f
DamataDeveloper <damatadeveloper@gmail.com>
2022-10-25 18:05:43 -0300

    alteracao e criacao tabelas usuarios e imc

--- src/dataBase/TabelaInfra.js
+++ src/dataBase/TabelaInfra.js
@@ -2,15 +2,26 @@ class TabelaInfra{
     static init(conexao){
         this.conexao = conexao;
         this.criaTabelaUsuario();
+        this.criaTabelaIMC();
 
     }
     static criaTabelaUsuario(){
-        const sql = 'CREATE TABLE IF NOT EXISTS usuario (id INT NOT NULL AUTO_INCREMENT, nome VARCHAR(55) NOT NULL, idade INT NOT NULL, imc FLOAT NOT NULL, data DATE NOT NULL, status INT NOT NULL, PRIMARY KEY (id) )'
+        const sql = 'CREATE TABLE IF NOT EXISTS usuarios (id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(55) NOT NULL, idade INT NOT NULL,data DATE NOT NULL, status INT NOT NULL)'
         this.conexao.query(sql, erro =>{
             if(erro){
                 console.log(erro + "[Usuarios]");
             }else{
                 console.log('Tabela [Usuarios] criada com sucesso');
+            }
+        })
+    }
+    static criaTabelaIMC(){
+        const sql = 'CREATE TABLE IF NOT EXISTS imc (id_imc INT NOT NULL AUTO_INCREMENT  PRIMARY KEY, imc FLOAT NOT NULL, altura FLOAT NOT NULL, peso FLOAT NOT NULL, data DATE NOT NULL, fk_Usuarios INT NOT NULL,FOREIGN KEY(fk_Usuarios) REFERENCES usuarios(id_usuario))'
+        this.conexao.query(sql, erro =>{
+            if(erro){
+                console.log(erro + "[IMC]");
+            }else{
+                console.log('Tabela [IMC] criada com sucesso');
             }
         })
     }
